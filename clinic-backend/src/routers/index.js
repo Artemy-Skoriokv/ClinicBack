b0a33c9b311b2704a78050451516af8e7d6a9cef
artem/s <a.skorikov.fortech@gmail.com>
2026-04-01 21:53:53 +0300

    feat: update refresh token

--- clinic-backend/src/routers/index.js
+++ clinic-backend/src/routers/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const appointmentRoutes = require("./appointment");
 const authRoutes = require("./auth");
 
 router.use("/auth", authRoutes);
-router.use("/appointments", appointmentRoutes);
 
 module.exports = router;
